93a5ebe6adb5be181cfa85d52f2e919ca195794a
Ken Hughes <ken@kapie.com>
2018-09-12 22:58:03 +0100

    Added debug to test app.

--- mpu6050-test.cpp
+++ mpu6050-test.cpp
@@ -5,16 +5,29 @@
  *
  */
 
+#include <stdio.h>
+#include <string.h>
 
- #include "mpu6050.h"
 
- int main()
- {
-     MPU6050 *mpu = new MPU6050();
-     if (mpu->whoAmI())
-     {
-         mpu->setBypassEnable(false);
-         return 1;
-     }
-     return 0;
- }
+#include "mpu6050.h"
+
+int main()
+{
+    MPU6050 *mpu = new MPU6050();
+    if (mpu->whoAmI())
+    {
+        printf("WhoAmI was okay\n");
+        if (mpu->getBypassEnable())
+        {
+            printf("Bypass is ENABLED\n");
+        }
+        else
+        {
+            printf("Bypass is DISABLED\n");
+        }
+        printf("Toggling...\n");
+        mpu->setBypassEnable(mpu->getBypassEnable());
+        return 1;
+    }
+    return 0;
+}
